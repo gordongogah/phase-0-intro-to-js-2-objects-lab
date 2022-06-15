fa825ad549504c6d8afa8aadafa80a42a8515c1e
gordongogah <gordongogah9@gmail.com>
2022-06-16 00:11:14 +0300

    commit tree 1ac5b6428f1ec492b22c6d427fb12184c996f2bf

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway'
+}
+//console.log(typeof(employee))
+//console.log(Object.keys(employee))
+//console.log(Object.values(employee))
+function updateEmployeeWithKeyAndValue(key, value){
+    //  const newEmployee = {...employee}
+    //  newEmployee[key] = value;
+    //  return newEmployee
+    return{
+        ...employee,
+        [key]:value,
+    };
+    
+}
+//console.log(updateEmployeeWithKeyAndValue(employee,'age',35))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+}
